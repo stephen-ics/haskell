5c38d21d02325bac8fd773dddfdeab1399aad1c3
stephen-ics <stephenni1234@gmail.com>
2023-08-16 18:03:03 -0600

    added mini evaluator and REPL

--- app/parsec.hs
+++ app/parsec.hs
@@ -154,3 +154,53 @@ parens m = do
     return n
 --parens is a function that takes a parser 'm' as an argument, it uses the reserved function to parse an opening parenthesis '(', then it uses the parser 'm' to parse the value 'a' and bind it to 'n', then the closing parenthesis is parsed and the parsed value n is returned (this parses the parenthesis and automatically advances the parser to parse the next value)
 
+data Expr --these constructors represent different nodes in the ast for expressions
+    = Add Expr Expr --expr not int because you may be able to stack infix expressions like 1 + 1 + 1, 1 and 1 + 1 are both expressions, 1 being a Lit and 1 + 1 being an Add
+    | Mul Expr Expr
+    | Sub Expr Expr
+    | Lit Int
+    deriving Show
+
+eval :: Expr -> Int
+eval ex = case ex of
+    Add a b -> eval a + eval b
+    Mul a b -> eval a * eval b
+    Sub a b -> eval a - eval b
+    Lit n -> n
+
+int :: Parser Expr
+int = do
+    n <- number --reads the number and binds it to 'n'
+    return (Lit n) --parses the integer by wrapping the value in a 'Lit' constructor
+--both of these steps are fundemental to the parsing process
+
+expr :: Parser Expr
+expr = term `chainl1` addop
+
+term :: Parser Expr
+term = factor `chainl1` mulop
+
+factor :: Parser Expr
+factor =
+    int
+    <|> parens expr
+
+infixOp :: String -> (a -> a -> a) -> Parser (a -> a -> a)
+infixOp x f = reserved x >> return f
+
+addop :: Parser (Expr -> Expr -> Expr)
+addop = (infixOp "+" Add) <|> (infixOp "-" Sub)
+
+mulop :: Parser (Expr -> Expr -> Expr)
+mulop = infixOp "*" Mul
+
+run :: String -> Expr
+run = runParser expr
+--so pretty much the runParser parses the input into an expression
+--then run
+
+main :: IO ()
+main = forever $ do
+    putStr "> "
+    a <- getLine
+    print $ eval $ run a
